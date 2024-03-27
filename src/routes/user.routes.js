49d567454cca8cd546f203301c8a6f06f79f0a9f
Raghunath Das <raghunathdas704@gmail.com>
2024-03-27 16:30:06 +0530

    test

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -64,6 +64,7 @@ router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updat
 router. route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router. route("/history").get(verifyJWT, getWatchHistory)
 
+//test
 
 
 export default router
