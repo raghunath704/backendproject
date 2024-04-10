72bcc315d6d7dab7da8c727329b9044c6ec1c771
Raghunath Das <raghunathdas704@gmail.com>
2024-04-11 00:23:08 +0530

    Add dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,13 +8,111 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+    // search for your channel in the DB
+    const channel = await User.findById(req.user?._id);
+
+    if (!channel) {
+        throw new ApiError(400, "Channel not found.");
+    }
+
+    const channelStats = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channel),
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "video_likes",
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "owner",
+                foreignField: "channel",
+                as: "total_subscribers"
+            }
+        },
+        {
+           $group: {
+                _id: null,
+                TotalVideos: {$sum : 1},    // sum to get all videos of the user
+                TotalViews: {$sum : "$views"},    
+                TotalSubscribers: {
+                    $first: {
+                        $size: "$total_subscribers"
+                    }
+                },
+                TotalLikes: {
+                    $first: {
+                        $size: "$video_likes"
+                    }
+                },
+
+           }
+        },
+        {
+            $project: {
+               TotalVideos: 1,
+               TotalSubscribers: 1,
+               TotalViews: 1,
+               TotalLikes: 1,
+            }
+        }
+    ])
+
+    // return response
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channelStats, "Channel stats fetched successfully."))
+
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    // search for your channel in the DB
+    const channel = await User.findById(req.user?._id);
+
+    if (!channel) {
+        throw new ApiError(400, "Channel not found.");
+    }
+    const channelVideos=await Video.aggregate([
+        {
+            $match:{
+                owner:mongoose.Types.ObjectId(channel)
+            }
+        },
+        {
+            $project:{
+                _id: 1,
+                title: 1,
+                description: 1,
+                videoFile: 1,
+                thumbnail: 1,
+                createdAt: 1,
+
+            }
+        }
+    ])
+
+    if (channelVideos.length === 0) {
+        throw new apiError(404, "No videos were found")
+    }
+
+    // returning response
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channelVideos, "Channel videos fetched successfully."))
+
 })
 
 export {
     getChannelStats, 
     getChannelVideos
-    }
+}
